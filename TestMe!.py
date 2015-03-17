48e27b12d97181f4303f2dd1f039cb7b3fd932d9
programmer9 <powerman902@gmail.com>
2015-03-17 16:54:31 -0700

    first version of TestMe!
    first version of TestMe!

---
+++ TestMe!.py
@@ -0,0 +1,62 @@
+print('Welcome to TestMe!')
+print('Type in what you would like on the front of your flashcard.')
+
+flashfront = input()
+
+print ('-------------------------------')
+print( '|                             |')
+print( '|                             |')
+print( '|                             |')
+print( '|                             |')
+print( '|                             |')                  
+print(         flashfront                               )
+print( '|                             |')
+print( '|                             |')
+print( '|                             |') 
+print( '|                             |')
+print( '|                             |')
+print( '|                             |')
+print( '-------------------------------')
+
+
+print('Type in what you would like the back of your flashcard.')
+
+flashback = input()
+
+print('--------------------------------- ')
+print('|                                |')
+print('|                                |')
+print('|                                |')
+print('|                                |')
+print(            flashback               )
+print('|                                |')
+print('|                                |')
+print('|                                |')
+print('|                                |')
+print('|                                |')
+print('|                                |')
+print('----------------------------------')
+
+
+
+print('Would you like to see your flashcards?')
+ans = input()
+if ans =='yes':
+    print('front or back?')
+fb = input()
+if fb == 'front':
+    print( '-------------------------------')
+    print( '|                             |')
+    print( '|                             |')
+    print( '|                             |')
+    print( '|                             |')
+    print( '|                             |')                  
+    print(         flashfront                                )
+    print( '|                             |')
+    print( '|                             |')
+    print( '|                             |') 
+    print( '|                             |')
+    print( '|                             |')
+    print( '|                             |')
+    print( '-------------------------------')
+
